6f1c051c89f76c17857370e7f52818a8b4ef44e1
Raymon Guzman <frankguzman23@gmail.com>
2019-03-22 18:09:46 -0400

    Using PropType.shape

--- src/components/WeatherLocation/WeatherData/index.jsx
+++ src/components/WeatherLocation/WeatherData/index.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import './styles.css';
-import {
-  CLOUD,
-  CLOUDY,
-  SUN,
-  RAIN,
-  SNOW,
-  WINDY,
-} from '../../../constants/weather';
 
-const WeatherData = () => (
-  <div className="weatherDataCont">
-    <WeatherTemperature temperature={20} weatherState={RAIN} />
-    <WeatherExtraInfo humidity={80} wind="10m/s" />
 
-  </div>
-);
+const WeatherData = ({ data }) => {
+  const {
+    humidity, wind, temperature, weatherState,
+  } = data;
+  return (
+    <div className="weatherDataCont">
+      <WeatherTemperature temperature={temperature} weatherState={weatherState} />
+      <WeatherExtraInfo humidity={humidity} wind={wind} />
+
+    </div>
+  );
+};
+
+WeatherData.propTypes = {
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired,
+
+    }),
+
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -3,12 +3,29 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
 
+import {
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY,
+} from '../../constants/weather';
+
+const data = {
+  temperature: 30,
+  weatherState: SUN,
+  humidity: 10,
+  wind: '10 m/s',
+
+};
+
 const WeatherLocation = () => (
   <div className="weatherLocationCont">
     <Location city="Buenos Aires!" />
-
-    <WeatherData />
+    <WeatherData data={data} />
   </div>
 );
+
 
 export default WeatherLocation;
