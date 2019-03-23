d7b906f4198b1ccc804c6b524bcf19aa328ea2ad
Raymon Francisco Guzmán Domínguez <rguzman@viva.com.do>
2019-03-23 15:14:57 -0400

    Fixing tiny bugs

--- src/components/WeatherLocation/WeatherData/index.jsx
+++ src/components/WeatherLocation/WeatherData/index.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import './styles.css';
-import {
-  CLOUD,
-  CLOUDY,
-  SUN,
-  RAIN,
-  SNOW,
-  WINDY,
-} from '../../../constants/weather';
 
-const WeatherData = () => (
-  <div className="weatherDataCont">
-    <WeatherTemperature temperature={20} weatherState={RAIN} />
-    <WeatherExtraInfo humidity={80} wind="10m/s" />
 
-  </div>
-);
+const WeatherData = ({ data }) => {
+  const {
+    temperature, weatherState, humidity, wind,
+  } = data;
+  return (
+    <div className="weatherDataCont">
+      <WeatherTemperature temperature={temperature} weatherState={weatherState} />
+      <WeatherExtraInfo humidity={humidity} wind={wind} />
+
+    </div>
+  );
+};
+
+WeatherData.propTypes = {
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired,
+  }),
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.jsx
+++ src/components/WeatherLocation/index.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY,
+} from '../../constants/weather';
 import './styles.css';
+
+const data = {
+  temperature: 20,
+  weatherState: SUN,
+  humidity: 10,
+  wind: '10 m/s',
+
+};
 
 const WeatherLocation = () => (
   <div className="weatherLocationCont">
     <Location city="Buenos Aires!" />
 
-    <WeatherData />
+    <WeatherData data={data}/>
   </div>
 );
+
+
 
 export default WeatherLocation;
