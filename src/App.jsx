63747457478dabd2ba5b05403f2f73a3a715221e
Raymon Francisco Guzmán Domínguez <rguzman@viva.com.do>
2019-04-14 15:02:17 -0400

    Installing React Flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LocationList from './components/LocationList';
 import './App.css';
@@ -21,11 +22,29 @@ class App extends Component {
     return (
 
       <MuiThemeProvider>
-        <div className="App">
+     <Grid fluid>
+        <Row>
+          <Col xs={12} sm= {6} md={4} lg={3}>
+          <div className="red">Hello, world!</div>
+          </Col>
+          <Col xs={12} sm= {6} md={4} lg={3}>
+            <div className="green">Hello, world!</div>            
+          </Col>
+          <Col xs={12} sm= {6} md={4} lg={3}>
+            <div className="blue">Hello, world!</div>
+          </Col>
+          <Col xs={12} sm= {6} md={4} lg={3}>
+            <div className="yellow">Hello, world!</div>
+          </Col>
+        </Row>
+        
+      </Grid>
+     
+     {/*    <div className="App">
 
           <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}/>
 
-        </div>
+        </div> */}
       </MuiThemeProvider>
     );
   };
